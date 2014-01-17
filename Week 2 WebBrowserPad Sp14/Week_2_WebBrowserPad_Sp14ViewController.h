f1941f2aec99909c420e57ff23244291569ac703
Chuck Konkol <ckonkol@gmail.com>
2014-01-17 17:37:21 -0600

    Week 2 WebBrowserPad Assignment
    Week 2 WebBrowserPad Assignment

---
+++ Week 2 WebBrowserPad Sp14/Week_2_WebBrowserPad_Sp14ViewController.h
@@ -0,0 +1,16 @@
+//
+//  Week_2_WebBrowserPad_Sp14ViewController.h
+//  Week 2 WebBrowserPad Sp14
+//
+//  Created by Charles Konkol on 1/17/14.
+//  Copyright (c) 2014 Chuck Konkol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Week_2_WebBrowserPad_Sp14ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *txtURL;
+- (IBAction)btnGo:(id)sender;
+@property (strong, nonatomic) IBOutlet UIWebView *webview;
+
+@end
